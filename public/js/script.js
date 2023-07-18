eba099da20c2d8e1f5215ee2d1cf1dc01cee86ac
Gábor Farkas <farkas.gz@gmail.com>
2023-07-18 16:56:05 +0200

    did things

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,17 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("taskManager JS imported successfully!");
 
   const allTasks = document.querySelectorAll(".added-task")
 
   allTasks.forEach(task => {
-    task.addEventListener("click", () => {
-      task.classList.toggle("crossed-task");
+    task.addEventListener("click", async () => {
+      try {
+        await fetch(`/home/task/${task.id}`, {method: 'POST'});
+      } catch (error) {
+        console.log(error);
+      }
     })
   })
-  
 });
